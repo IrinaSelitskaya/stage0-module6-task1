f32b4946306741868de0f823374f8083915eb977
IrinaSelitskaya <119079534+IrinaSelitskaya@users.noreply.github.com>
2023-02-21 21:14:38 +0300

    Update Bird.java

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -3,6 +3,19 @@ package com.epam.mjc.stage0;
 public class Bird extends Animal{
  public Bird(){
   super("blue",2,false);
-}
-
+ }
+ public String getDescription(){
+  String x;
+  String y;
+  if(numberOfPaws==1){
+   x="paw";
+  }else{
+   x="paws";} 
+  if(hasFur==true){
+   y="a";
+  }else{
+   y="no";} 
+  String description="This animal is mostly "+color+". It has "+numberOfPaws+" "+x+" and "+y+" fur. Moreover, it has 2 wings and can fly."; 
+  return description;
+ } 
 }
