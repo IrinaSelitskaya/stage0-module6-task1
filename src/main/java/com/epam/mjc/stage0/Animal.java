c28633f6d3a021e46af2a689d462aef29ee3555d
IrinaSelitskaya <119079534+IrinaSelitskaya@users.noreply.github.com>
2023-02-21 11:50:59 +0300

    Update Animal.java

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,19 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
-
+ private String color;
+ private int numberOfPaws;
+ private boolean hasFur;
+ public Animal(String color,int numberOfPaws,boolean hasFur){
+  this.color=color;
+  this.numberOfPaws=numberOfPaws;
+  this.hasFur=hasFur;} 
+ public String getDescription(){
+  String description;
+  if(hasFur==true){
+   description="This animal is mostly "+color+". It has "+numberOfPaws+" paws and a fur.";
+  }else{
+   description="This animal is mostly "+color+". It has "+numberOfPaws+" paws and no fur.";} 
+  return description;
+ } 
 }
